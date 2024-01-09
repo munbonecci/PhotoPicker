a80db7006db21e7be88a6a4954b60b1a7cfe2c7e
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2024-01-09 16:52:21 -0600

    unused code removed

--- app/src/main/java/com/mun/bonecci/photopicker/ui/theme/Dimens.kt
+++ app/src/main/java/com/mun/bonecci/photopicker/ui/theme/Dimens.kt
@@ -2,16 +2,10 @@ package com.mun.bonecci.photopicker.ui.theme
 
 import androidx.compose.ui.unit.dp
 
-val dimen_0dp = 0.dp
-val dimen_2dp = 2.dp
+
 val dimen_5dp = 5.dp
-val dimen_8dp = 8.dp
 val dimen_10dp = 10.dp
 val dimen_14dp = 14.dp
 val dimen_16dp = 16.dp
 val dimen_18dp = 18.dp
-val dimen_20dp = 20.dp
-val dimen_30dp = 30.dp
-val dimen_64dp = 64.dp
-val dimen_100dp = 100.dp
 val dimen_200dp = 200.dp
